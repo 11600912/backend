9babb65632fa7888ea9bc4ce655a1223fd0f023e
11600912 <m.divesh98@gmail.com>
2018-07-17 18:07:15 +0530

    db configured

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
 module.exports = {
-    database:'mongodb://localhost:27017/backend',
+    //database:'mongodb://localhost:27017/backend',
+    database:'mongodb://divesh:divesh123@ds047592.mlab.com:47592/heroku_3sk9zn3l',
+
     secret:'yoursecret'
 }
